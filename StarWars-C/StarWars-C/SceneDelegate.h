e80e1446fda577c21ebfd653c433c5d381e87fba
LAURA JELENICH <laurajelenich@LAURAs-MacBook-Pro-2.local>
2020-10-01 14:46:53 -0400

    first commit

---
+++ StarWars-C/StarWars-C/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  StarWars-C
+//
+//  Created by LAURA JELENICH on 10/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ StarWars-C/StarWars-C/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  StarWars-C
+//
+//  Created by LAURA JELENICH on 10/1/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
